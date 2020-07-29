cfbc8134e2dce0623704e7d7cc5fab8d2a01899a
Eddie Jaoude <eddie@jaoudestudios.com>
2020-07-29 15:12:03 +0100

    angular + tailwindcss + basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+
+import { faArrowRight, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faYoutubeSquare, faLinkedin, faTwitterSquare, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +14,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(
+      faArrowRight, faQuoteLeft, faQuoteRight,
+      faGithubSquare, faYoutubeSquare, faLinkedin, faTwitterSquare, faDiscord,
+    );
+  }
+}
